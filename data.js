fac5e67ef7d48ab334c82325aca2e123c1c07e1c
xavipaulito <pablymff@gmail.com>
2020-09-21 12:03:47 -0300

    upload files

---
+++ data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pano_20200921_100533_0",
+      "name": "PANO_20200921_100533_0",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.2524378654369617,
+        "pitch": 0,
+        "fov": 1.2342559994395403
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.2077696548623571,
+          "pitch": 0.10841615823732553,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
